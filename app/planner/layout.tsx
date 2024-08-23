b26f5e03cdd9d0e6f6b2030dc413e56f0a350824
Aurelien B. <61365539+Neltab@users.noreply.github.com>
2024-08-23 23:33:12 +0200

    fix: login call to action position

--- app/planner/layout.tsx
+++ app/planner/layout.tsx
@@ -2,8 +2,28 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/
 import VacationList from "../../components/planner/VacationList";
 import { Skeleton } from "@/components/ui/skeleton";
 import { TbBeach } from "react-icons/tb";
+import SignInButton from "@/components/planner/SignInButton";
+import { getServerSession } from "next-auth";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession();
+
+  if (!session) {
+    return (
+      <div className="flex flex-row min-h-screen">
+        <div className="shadow flex flex-col p-4 gap-4 w-[256px]">
+          <Skeleton className="h-7 w-full" />
+          <Skeleton className="h-7 w-full" />
+          <Skeleton className="h-7 w-full" />
+          <div className="flex flex-col items-center max-w-full gap-4">
+            <h1>Connectez vous pour accéder à vos vacances</h1>
+            <SignInButton />
+          </div>
+        </div>
+        {children}
+      </div>
+    )
+  }
   return (
     <div className="flex flex-row min-h-screen">
       <div className="shadow flex flex-col p-4 gap-4 w-[256px]">
--- components/planner/VacationList.tsx
+++ components/planner/VacationList.tsx
@@ -1,21 +1,8 @@
 import { getMyVacations } from "@/app/api/vacations/Providers/server";
-import { getServerSession } from "next-auth"
 import Link from "next/link";
-import SignInButton from "./SignInButton";
 import NewVacationButton from "./vacations/NewVacationButton";
 
 export default async function VacationList() {
-  const session = await getServerSession();
-
-  if (!session) {
-    return (
-      <div className="flex flex-col items-center max-w-full gap-4">
-        <h1>Connectez vous pour accéder à vos vacances</h1>
-        <SignInButton />
-      </div>
-    )
-  }
-
   const myVacations = await getMyVacations();
 
   return (
